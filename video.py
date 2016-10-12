a49fc323d7e1161e14734e345e7a09ccd7e748be
Diego Dukão <diegodukao@gmail.com>
2016-10-12 17:31:20 -0300

    video with cover

--- video.py
+++ video.py
@@ -1,16 +1,24 @@
 from kivy.factory import Factory
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from kivy.uix.video import Video as KivyVideo
 
 Builder.load_file('video.kv')
 
 
 class Video(KivyVideo):
+    image = ObjectProperty()
 
     def on_state(self, instance, value):
-        if self.state == 'stop':
+        if self.state == 'play':
+            self.cover.opacity = 0
+        elif self.state == 'stop':
             self.seek(0)
+            self.cover.opacity = 1
         return super(self.__class__, self).on_state(instance, value)
+
+    def on_image(self, instance, value):
+        self.cover.opacity = 1
 
     def on_eos(self, instance, value):
         if value:
