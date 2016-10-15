10b045e1f03c1909a76dd7b33fb0034937892f55
Diego Dukão <diegodukao@gmail.com>
2016-10-14 22:00:33 -0300

    controlbar working

---
+++ videocontroller.py
@@ -0,0 +1,11 @@
+from kivy.uix.floatlayout import FloatLayout
+from kivy.lang import Builder
+
+import video
+import controlbar
+
+Builder.load_file('videocontroller.kv')
+
+
+class VideoController(FloatLayout):
+    pass
