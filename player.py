dd6b200e9c9bc4c9297f5c6a1a7e49fe26ce2689
Diego Dukão <diegodukao@gmail.com>
2016-10-25 13:32:44 -0200

    alternatively using kivy videoplayer widget

--- player.py
+++ player.py
@@ -1,5 +1,13 @@
 from kivy.app import App
 from videocontroller import VideoController
+# from kivy.uix.videoplayer import VideoPlayer
+#
+#
+# class MyVideoPlayer(VideoPlayer):
+#     def __init__(self, **kwargs):
+#         self.source = "2-1-diagram-beautification.mp4"
+#         self.state = 'play'
+#         super().__init__(**kwargs)
 
 
 class Kvve(App):
